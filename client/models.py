3b346df1209dc0015f9768f24450e1244fe21221
dougtc1 <douglastorres.usb@gmail.com>
2016-12-15 23:19:07 -0400

    finished app

--- client/models.py
+++ client/models.py
@@ -1,28 +1,38 @@
 from django.db import models
+from datetime import datetime
 
 class Client(models.Model):
     name = models.CharField(max_length=200, verbose_name=('Nombre'))
     last_name = models.CharField(max_length=200, verbose_name=('Apellido'))
     age = models.IntegerField(verbose_name=('Edad'))
-    id_number = models.CharField(max_length=200, verbose_name=('Cedula'))
+    id_number = models.CharField(max_length=200, verbose_name=('Cedula'), unique=True)
     password = models.CharField(max_length=200, verbose_name=('Password'))
-    nickname = models.CharField(max_length=200, unique=True, verbose_name=('Nickname'))
+    nickname = models.CharField(max_length=200, primary_key=True, verbose_name=('Nickname'))
     type_Client =  models.IntegerField(verbose_name=('Valor'), default=0)
     points = models.IntegerField(verbose_name=('Saldo'), default=3000)
 
     def __str__(self):
         return "%s %s" % (self.name,self.last_name) 
 
+    def addPaid(self, monto):
+        self.points += monto
+        return self.points
+
+    def lessPaid(self, monto):
+        self.points -= monto
+        return self.points        
+
     class Meta:
         verbose_name = ('Usuario')
         verbose_name_plural = ('Usuarios')
 
+
 class Transaction(models.Model):
     id_transaction = models.IntegerField(verbose_name=('Numero de transaccion'))
-    fecha = models.DateTimeField(verbose_name=("Fecha y hora de transaccion"))
+    fecha = models.DateTimeField(verbose_name=("Fecha y hora de transaccion"), default=datetime.now)
     remitente = models.ForeignKey('Client', related_name='cliente1',null=True)
     receptor = models.ForeignKey('Client', related_name='cliente2',null=True)
-    concepto = models.CharField(max_length=200, verbose_name=('Concepto'))
+    concepto = models.CharField(max_length=200, verbose_name=('Concepto'), null=True)
     monto = models.IntegerField(verbose_name=('Monto de transferencia'),null=True)
 
     def __str__(self):
--- client/viewsets.py
+++ client/viewsets.py
@@ -5,6 +5,9 @@ from rest_framework.permissions import AllowAny
 from .models import Client, Transaction
 from .serializers import ClientSerializer, TransactionSerializer
 from django.core import serializers
+from datetime import datetime
+
+contador = 0
 
 class ClientViewSet(viewsets.ModelViewSet):
     permission_classes = (AllowAny, )
@@ -48,7 +51,6 @@ class GeneralLog(APIView):
                     return Response(answerData, status.HTTP_412_PRECONDITION_FAILED)
                 else:
                     del client['password']
-                    del client['id']
                     answerData = {'id': 1, 'client':client}
                     return Response(answerData, status.HTTP_200_OK)                
             elif(action==1 or action==2):
@@ -61,11 +63,6 @@ class GeneralLog(APIView):
                 id_number = data['id_number']
                 password = data['password']
                 nickname = data['nickname']
-
-                client = Client.objects.filter(id_number=id_number)
-                if client:
-                    answerData = {'id':-1, 'response':'Id de usuario ya registrado'}
-                    return Response(answerData, status.HTTP_412_PRECONDITION_FAILED)
 
                 client = Client.objects.filter(nickname=nickname)
                 if client:
@@ -82,9 +79,45 @@ class GeneralLog(APIView):
                 client = Client.objects.filter(nickname=nickname)
                 client = client.values()[0]
                 del client['password']
-                del client['id']
 
                 answerData = {'id': 1, 'client':client}
                 return Response(answerData, status.HTTP_200_OK)
+        except:
+            return Response({request},status.HTTP_412_PRECONDITION_FAILED)
+
+class PaidView(APIView):
+    """View for login and logup"""
+    permission_classes = (AllowAny, )
+
+    def post(self, request, *arg, **kargs):
+
+        try:
+            data = request.data
+            user = data["nickname_remitente"]
+            nickname = data["nickname_receptor"]
+            concepto = data["concepto"]
+            monto = int(data["monto"])
+        
+            client = Client.objects.get(nickname=user)
+            client2 = Client.objects.get(nickname=nickname)
+            if not client2:
+                answerData = {'id':-1, 'response':'No existe el usuario'}
+                return Response(answerData, status.HTTP_412_PRECONDITION_FAILED)
+
+            client.points -= monto
+            client2.points += monto
+
+            client.save(update_fields=['points'])
+            client2.save(update_fields=['points'])
+            
+            print(client.points)
+            print(client2.points)
+            global contador
+            contador += 1
+            transaction = Transaction(id_transaction=contador,remitente=client, receptor=client2, concepto=concepto, monto=monto)
+            transaction.save()
+            answerData = {'id': 1, 'message':'Transacción exitosa'}
+            return Response(answerData, status.HTTP_200_OK)
+        
         except:
             return Response({request},status.HTTP_412_PRECONDITION_FAILED)
